42428015661ccd950bc0abeb73ef905c7a476645
Nawaz Muhammad <mack@MacBook-Pro.local>
2024-11-04 16:10:57 -0500

    modified script file

--- scripts/build_application.sh
+++ scripts/build_application.sh
@@ -2,4 +2,4 @@
 # Navigate to the application folder, install dependencies, and build the Angular app
 cd /var/www/html/angular-app # Adjust this path if needed
 npm install
-ng build --prod # This will create a production build in the 'dist' folder
+ng build --configuration production # This will create a production build in the 'dist' folder
